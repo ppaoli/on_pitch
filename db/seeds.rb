06ff73350395cadf02b8f6fe31b9042d443d4427
Alice Heber <alice.heber@gmail.com>
2022-11-22 11:38:13 +0000

    installed faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |i|
+  Pitch.create(name: "Pitch", sport: "type of sport", address: "street postcode", reference: "#{random(1..10)}")
+end

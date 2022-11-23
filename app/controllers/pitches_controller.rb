e4700824ef3d0554990b7fc7925a20a1d53ead3f
philippe paoli <philippe.paoli11@gmail.com>
2022-11-23 12:53:04 +0000

    added upload form for a new pitch

--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -6,4 +6,22 @@ class PitchesController < ApplicationController
   def show
     @pitch = Pitch.find(params[:id])
   end
+
+  def new
+    @pitch = Pitch.new
+  end
+
+  def create
+    @pitch = Pitch.new(pitch_params)
+    if @pitch.save
+      redirect_to pitch_path(@pitch)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+    private
+  def pitch_params
+    params.require(:pitch).permit(:name, :address, :sport, :photo)
+  end
 end
